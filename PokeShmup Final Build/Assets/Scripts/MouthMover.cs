1416fd65bde91e5bd20f0b8e0b9229ca7e3ad2a9
dizzyupthegirl <wolfsp@miamioh.edu>
2014-11-18 17:48:18 -0500

    Updated Scary Face
    Scary face is farther back now, moves slower, and the mouth spins after
    a few seconds to add some difficulty

--- PokeShmup Final Build/Assets/Scripts/MouthMover.cs
+++ PokeShmup Final Build/Assets/Scripts/MouthMover.cs
@@ -5,20 +5,24 @@ using System.Collections;
 public class MouthMover : MonoBehaviour {
 
 	public float speed;
-
+	private bool spinning;
+	private float startTime;
 	// Use this for initialization
 	void Start () {
-		//waits ();
+		waits ();
+		startTime = Time.time;
 		rigidbody.velocity = transform.forward * speed;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if((Time.time-startTime)>1.0)
+			transform.Rotate(0, 180* Time.deltaTime, 0, Space.World);
 	
 	}
-	//IEnumerator waits(){
-		//yield return new WaitForSeconds (4.0f);
-	//	rigidbody.velocity = transform.forward * speed;
+	IEnumerator waits(){
+		yield return new WaitForSeconds (4.0f);
 	
-	//}
+	}
 }
