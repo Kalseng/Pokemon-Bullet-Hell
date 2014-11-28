2bba6b11820f6be45cdf4f2bf2aa278156a2492c
dizzyupthegirl <wolfsp@miamioh.edu>
2014-11-28 14:29:28 -0500

    Working on High Score
    Trying to see what I can make happen here…

--- PokeShmup Final Build/Assets/Scripts/LogicOfTheGame.cs
+++ PokeShmup Final Build/Assets/Scripts/LogicOfTheGame.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Text;
+using System.IO;  
 
 public class LogicOfTheGame : MonoBehaviour {
 
@@ -23,11 +25,14 @@ public class LogicOfTheGame : MonoBehaviour {
 	public GameObject fader, fader2;
 	public float speed;
 	private Color faderColor, faderColor2;
+	private string[] highscores;
+
 
 	// Use this for initialization
 
 
 	void Start () {
+
 		if (Application.loadedLevelName == "Level 1") {
 						powerUp = 0.0f;
 						
@@ -35,7 +40,7 @@ public class LogicOfTheGame : MonoBehaviour {
 				}
 		bullets = GameObject.Find ("Bullets");
 				if (lives == 0) {
-						lives = 3;
+						lives = 50;
 				}gameOver = false;
 		level1Over = false;    
 				scoreText.text = "Score: " + score;
@@ -177,4 +182,56 @@ public class LogicOfTheGame : MonoBehaviour {
 		powerUp += change;
 		}
 	}
+
+	private bool Load(string fileName)
+	{
+		// Handle any problems that might arise when reading the text
+		try
+		{
+			string line;
+			// Create a new StreamReader, tell it which file to read and what encoding the file
+			// was saved as
+			StreamReader theReader = new StreamReader(fileName, Encoding.Default);
+			
+			// Immediately clean up the reader after this block of code is done.
+			// You generally use the "using" statement for potentially memory-intensive objects
+			// instead of relying on garbage collection.
+			// (Do not confuse this with the using directive for namespace at the 
+			// beginning of a class!)
+			using (theReader)
+			{
+				// While there's lines left in the text file, do this:
+				do
+				{
+					line = theReader.ReadLine();
+					
+					if (line != null)
+					{
+						// Do whatever you need to do with the text line, it's a string now
+						// In this example, I split it into arguments based on comma
+						// deliniators, then send that array to DoStuff()
+						highscores = line.Split(',');
+						if (highscores.Length > 0)
+							DoStuff(entries);
+					}
+				}
+				while (line != null);
+				
+				// Done reading, close the reader and return true to broadcast success    
+				theReader.Close();
+				return true;
+			}
+		}
+		
+		// If anything broke in the try block, we throw an exception with information
+		// on what didn't work
+		catch (Exception e)
+		{
+			Console.WriteLine("{0}\n", e.Message);
+			return false;
+		}
+	}
+
+
+
 }
